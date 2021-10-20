a4036be2f30b564dbc0f92ff22441dbea196aef3
Liang C <sl20130313@gmail.com>
2021-10-20 22:58:27 +0800

    chore : set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', 'jsx'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
