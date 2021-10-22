ff733765da17ec528fdce12393a539830f6dff6b
Liang C <sl20130313@gmail.com>
2021-10-22 18:01:52 +0800

    feat : import GlobalStyle and try to fetch decard api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from 'react';
+import GlobalStyle from './styles/globalStyle';
+
 const App = () => {
-  return <div>Hello World !</div>;
+  useEffect(() => {
+    fetch('http://localhost:3000/v2/posts')
+      .then(data => {
+        return data.json();
+      })
+      .then(data => console.log(data));
+  });
+  return (
+    <>
+      <GlobalStyle />
+      <div>Hello World !</div>
+    </>
+  );
 };
 
 export default App;
