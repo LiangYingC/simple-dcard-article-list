ae0ce5351c90929d81946e2a7cd8e776cf9f0fdd
Liang C <sl20130313@gmail.com>
2021-10-24 17:23:34 +0800

    feat : implement simple article list with title and excerpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
-import { useEffect } from 'react';
+import { FC, useState, useEffect } from 'react';
+import styled from 'styled-components';
 import GlobalStyle from './styles/globalStyle';
 
-const App = () => {
+const Articles = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  background-color: #ffffff;
+`;
+
+const Article = styled.article`
+  position: relative;
+  margin: 0px 60px;
+  padding: 20px 0px;
+  border-bottom: 1px solid rgb(233, 233, 233);
+`;
+
+const Title = styled.h2`
+  color: #333333;
+`;
+
+const Excerpt = styled.p`
+  color: #555555;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+const App: FC = () => {
+  const [articles, setArticles] = useState([]);
+
   useEffect(() => {
-    fetch('http://localhost:3000/v2/posts')
+    fetch('http://localhost:3000/v2/posts?popular=true')
       .then(data => {
         return data.json();
       })
-      .then(data => console.log(data));
-  });
+      .then(data => {
+        setArticles(data);
+      });
+  }, []);
   return (
     <>
       <GlobalStyle />
-      <div>Hello World !</div>
+      <Articles>
+        {articles.map(({ id, title, excerpt }) => {
+          return (
+            <Article key={id}>
+              <Title>{title}</Title>
+              <Excerpt>{excerpt}</Excerpt>
+            </Article>
+          );
+        })}
+      </Articles>
     </>
   );
 };
