f9787f1b61e2691753167dee43e8b58e0381daad
Liang C <sl20130313@gmail.com>
2021-10-26 17:12:53 +0800

    feat : encapsulate newItems, fetchMoreItemsFn into useInfiniteScroll

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,7 +1,29 @@
 import { useEffect, useState } from 'react';
 
-const useInfiniteScroll = () => {
+interface UseInfiniteScrollParams<T> {
+  newItems: T[];
+  fetchMoreItemsFn: () => Promise<void>;
+}
+
+const useInfiniteScroll = <T>({ newItems, fetchMoreItemsFn }: UseInfiniteScrollParams<T>) => {
+  const [items, setItems] = useState<T[]>([]);
   const [isFetchMore, setIsFetchMore] = useState(false);
+
+  useEffect(() => {
+    setIsFetchMore(true);
+  }, []);
+
+  useEffect(() => {
+    setItems(prevItems => [...prevItems, ...newItems]);
+  }, [newItems]);
+
+  useEffect(() => {
+    if (isFetchMore) {
+      fetchMoreItemsFn().then(() => {
+        setIsFetchMore(false);
+      });
+    }
+  }, [isFetchMore, fetchMoreItemsFn]);
 
   function handleScroll() {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !isFetchMore) {
@@ -14,7 +36,7 @@ const useInfiniteScroll = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  return { isFetchMore, setIsFetchMore };
+  return { items, isFetchMore, setIsFetchMore };
 };
 
 export default useInfiniteScroll;
